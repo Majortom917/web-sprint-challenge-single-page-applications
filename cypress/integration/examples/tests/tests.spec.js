99ef94320c218e96c97bce2adfe86d9f31dcd052
Majortom917 <70175230+Majortom917@users.noreply.github.com>
2020-10-16 11:21:12 -0700

    styling"

--- cypress/integration/examples/tests/tests.spec.js
+++ cypress/integration/examples/tests/tests.spec.js
@@ -37,8 +37,8 @@ describe('testing', () => {
         it('can type inside the inputs', () => {
            nameInput()
               .should('have.value', '')
-              .type('ThrockMorton')
-              .should('have.value', 'ThrockMorton')
+              .type('Hue Janus')
+              .should('have.value', 'Hue Janus')
 
             specialInstruct()
             .should('have.value', '')
@@ -47,11 +47,11 @@ describe('testing', () => {
         })
       
         it('the submit button enables if we type on boths inputs', () => {
-            nameInput().type('foo')
+            nameInput().type('ThrockMorton')
             sizeInput().select('medium')
             pepperoniBox().check()
             bbqBox().check()
-           specialInstruct().type('bar')
+           specialInstruct().type('I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.')
             submitBtn().should('not.be.disabled')
           })
     })
